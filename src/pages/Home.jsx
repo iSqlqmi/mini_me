4d7761170bfb4136bf2ec29c0579dcf20de72d1f
iSqlqmi <selinajia1230@gmail.com>
2025-11-15 23:25:55 -0800

    added description and logo to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
 import { useNavigate } from "react-router-dom";
+import homepageLogo from "../images/Homepage_logo.png";
 
 export const Home = () => {
   return (
     <div>
-      <div>
+      <div className="title">
         <h1>Welcome to MiniMe!</h1>
+        <img src={homepageLogo} alt="Homepage Logo" width="800px" height="1000px"/>
       </div>
 
-      <div>
+      <div className="description">
+        <p>Hi welcome to MiniMe! MiniMe can help you keep track of your daily needs </p>
+        <p>sending you basic reminders to eat, sleep, and take a break! To get started </p>
+        <p>click on the "Get Started" button below to try MiniMe today!</p>
+      </div>
+
+      <div className="title-button">
         <GetStartedButton />
       </div>
     </div>
